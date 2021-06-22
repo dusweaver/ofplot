0d867a18fd80e5398770b4415ace16f1b3ca105d
dustin <dustinstevenweaver@gmail.com>
2021-06-21 20:07:15 -0700

    added OF v5.x to have similar sample line output as COM openFOAM

--- ofplot.py
+++ ofplot.py
@@ -17,7 +17,7 @@ class Configuration:
         else:
             self.target = target
 
-        if os.environ['WM_PROJECT_VERSION'][0] == 'v':
+        if os.environ['WM_PROJECT_VERSION'][0] == 'v' or os.environ['WM_PROJECT_VERSION'][0] == '5.x':
             self.version = 'COM'
         else:
             self.version = 'ORG'
@@ -67,7 +67,7 @@ class Configuration:
         if self.version == 'COM':
             string += 'type    uniform;' + '\n'
         else:
-            string += 'type    lineUniform;' + '\n'
+            string += 'type    uniform;' + '\n'
         string += 'axis    distance;' + '\n'
         string += 'nPoints 100;' + '\n'
         string += '}'
@@ -269,6 +269,3 @@ class Configuration:
                 for key_field in self.fields:
                     self.create_sample_plane(case, key_loc, value_loc, key_field)
         print('Sample files generated')
-
-#Dustin notes:
-# Ability to use run_parallel('blockMesh') before Configuration is done because get_domain_size() required polymesh
